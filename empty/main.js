d3838ab617a5834820f028077faa4f25194470f3
samanthajfern <samanthafern1@gmail.com>
2016-07-15 13:09:11 -0400

    change score

--- empty/main.js
+++ empty/main.js
@@ -25,6 +25,11 @@ var mainState = {
 
         this.pipes = game.add.group();
         this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+
+        this.score = 0;
+        this.labelScore = game.add.text(20, 20, "0",
+            { font: "30px Arial", fill: "#ffffff" });
+
         },
 
     update: function() {
@@ -34,6 +39,9 @@ var mainState = {
         // Call the 'restartGame' function
         if (this.bird.y < 0 || this.bird.y > 490)
         this.restartGame();
+
+        game.physics.arcade.overlap(
+        this.bird, this.pipes, this.restartGame, null, this);
     },
 
     jump: function() {
@@ -75,6 +83,9 @@ var mainState = {
         for (var i = 0; i < 8; i++)
             if (i != hole && i != hole + 1)
             this.addOnePipe(400, i * 60 + 10);
+
+            this.score += 1;
+            this.labelScore.text = this.score;
     },
 
 };
